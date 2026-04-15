3e18ef7193a2023b5f182f1a9797fae518afc3ad
yelei0 <yelei0@example.com>
2026-04-15 11:26:26 +0800

    Build project to dist folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '.', // 输出到根目录
-    emptyOutDir: false, // 不要清空根目录
-  },
 })
